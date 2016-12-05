2215e2ceeae9e5b803f39aa4714ccf1318973023
Ananth <agirijak@in.ibm.com>
2016-12-05 10:05:18 +0530

    Updated the Swift file

--- demoApp/AppDelegate.swift
+++ demoApp/AppDelegate.swift
@@ -22,10 +22,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-
 }
